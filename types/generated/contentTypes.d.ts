99ab68b8c9623cb5ba215644470d0ab81187d99d
buba <bub4dev@gmail.com>
2025-06-03 22:53:29 +0900

    feat: change seo type

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -425,7 +425,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
     > &
       Schema.Attribute.Private;
     publishedAt: Schema.Attribute.DateTime;
-    seo: Schema.Attribute.Component<'shared.seo', true>;
+    seo: Schema.Attribute.Component<'shared.seo', false>;
     slug: Schema.Attribute.UID<'title'>;
     title: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
